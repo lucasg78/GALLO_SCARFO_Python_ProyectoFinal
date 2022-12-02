80d35ad7f3e75f63b0608b14ac0f559dd883844a
Lucas Gallo <lucasgallo@gmail.com>
2022-12-02 11:56:17 -0300

    Corrigiendo errores en formularios y ajustando estilos

--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'ProyectoFinal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [f"{BASE_DIR}/ProyectoFinal/ProyectoFinal/AppBonos/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = (
+    "/AppBonos/inicio"  # URL redirecting after a successful authentication
+)
+
+LOGIN_URL = "/AppBonos/login"
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
