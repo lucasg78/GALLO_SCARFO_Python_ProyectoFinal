02c5c2f57d57e58dc1a974965078ef7591364406
Lucas Gallo <lucasgallo@gmail.com>
2022-12-06 17:14:28 -0300

    Agregando campo de fecha automática al blog

--- ProyectoFinal/AppBlog/models.py
+++ ProyectoFinal/AppBlog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Mensaje(models.Model):
+    fecha = models.DateField(default=date.today)
     nombre = models.CharField(max_length=30)
     email = models.EmailField()
-    mensaje = models.CharField(max_length=300)
+    mensaje = models.CharField(max_length=500)
 
     def __str__(self) -> str:
         return f"{self.nombre} {self.email}"
